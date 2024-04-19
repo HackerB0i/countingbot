5bcf26c4db2c128ed0ba8885f1172b792886c245
skonk <66183351+HackerB0i@users.noreply.github.com>
2024-04-18 21:04:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from dotenv import load_dotenv
 from discord.ext import commands, tasks
 from discord import Activity, ActivityType
 import time
-from ping import keep_alive
-client = commands.Bot(command_prefix = 'e!')
+client = commands.Bot(command_prefix = 'c!')
 
 @client.event
 async def on_ready():
-	print("egbot")
-	await client.change_presence(status = discord.Status.online,activity = discord.Game("eg"))
+	print("count bot initiated")
+	await client.change_presence(status = discord.Status.online,activity = discord.Game("counts numbers!"))
 	client.remove_command('help')
 @client.command
 async def count(countto, ctx):
 	for x in countto:
 		await ctx.send(x)
 keep_alive()
-client.run(os.getenv('token'))
+client.run(os.getenv('token')) # this is your bot's token in the discord applications thing
